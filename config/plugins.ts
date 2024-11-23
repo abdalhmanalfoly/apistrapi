db4f9685a93c8aa8bbb5ea8ce5fd206e10847c6a
abdalrhman <radea4082@gmail.com>
2024-11-23 16:08:13 +0200

    first commit

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,17 @@
-export default () => ({});
+export default ({ env }) => ({
+    upload: {
+      config: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: env('CLOUDINARY_CLOUD_NAME'),  // اسم حساب Cloudinary الخاص بك
+          api_key: env('CLOUDINARY_API_KEY'),       // مفتاح API الخاص بـ Cloudinary
+          api_secret: env('CLOUDINARY_API_SECRET'), // سر API الخاص بـ Cloudinary
+        },
+        actionOptions: {
+          upload: {}, // يمكن تركها فارغة
+          delete: {}, // يمكن تركها فارغة
+        },
+      },
+    },
+  });
+  
